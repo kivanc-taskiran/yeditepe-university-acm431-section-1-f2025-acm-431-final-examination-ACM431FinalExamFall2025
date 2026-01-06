7814fa397a9361872eea3d8d7688575f3ed38b8f
kivanc-taskiran <kivancmetetaskiran@gmail.com>
2026-01-06 15:27:32 +0300

    Update TaskRepository.kt

--- app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
+++ app/src/main/java/com/yeditepe/finalexam/repository/TaskRepository.kt
@@ -7,7 +7,17 @@ class TaskRepository(private val api: TaskApi) {
 
     suspend fun fetchTasks(): List<Task> {
         // TODO 1: Call API
+
+
+
+
+
         // TODO 2: Convert TaskDto list to Task list
-        return TODO("Provide the return value")
+
+
+
+
+
+        return emptyList()
     }
 }
