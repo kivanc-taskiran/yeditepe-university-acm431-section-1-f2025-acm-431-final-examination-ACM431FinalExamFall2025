6f3c7292a4dca2340b5d84c9004e4a6dc7df7bb3
kivanc-taskiran <kivancmetetaskiran@gmail.com>
2026-01-06 15:29:50 +0300

    Update TaskListScreen.kt

--- app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
+++ app/src/main/java/com/yeditepe/finalexam/ui/TaskListScreen.kt
@@ -2,6 +2,9 @@ package com.yeditepe.finalexam.ui
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -11,23 +14,36 @@ import com.yeditepe.finalexam.model.Task
 import com.yeditepe.finalexam.viewmodel.TaskViewModel
 
 @Composable
-fun TaskListScreen(viewModel: TaskViewModel = viewModel()) {
+fun TaskListScreen(navController: NavController, viewModel: TaskViewModel = viewModel()) {
 
     // TODO 3: Read task list from ViewModel
+    val tasks = viewModel.tasks
 
     Column {
         // TODO 4: Display task titles and completion status
-        // Use a simple Column or LazyColumn
+
+
+        LazyColumn {
+            items(tasks) { task ->
+                TaskRow(task = task, navController = navController)
+            }
+        }
     }
 }
 
 @Composable
 fun TaskRow(task: Task, navController: NavController) {
 
-    Text(
-        text = task.title,
-        modifier = Modifier.clickable {
-            // TODO 3: Navigate to detail screen with task title
-        }
-    )
+    Row(modifier = Modifier.clickable {
+        // TODO 3: Navigate to detail screen with task title
+        navController.navigate("taskDetail/${task.title}")
+    }) {
+        Text(
+            text = task.title,
+            modifier = Modifier.weight(1f)
+        )
+        Text(
+            text = if (task.isCompleted) "Completed" else "Not Completed"
+        )
+    }
 }
